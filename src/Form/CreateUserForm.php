8922a5bbbf7e39e752b6b077e8bb9e185374533e
Richard <164242635+DCL-P@users.noreply.github.com>
2025-06-25 19:15:42 +0200

    created the user creation form
    With this form class, the user can easily create a new account inside of the users entity (table)
    
    Note that some parts are a little hardcoded since i was just focussing on getting it setup.
    
    I'll clean up the code at a later time

---
+++ src/Form/CreateUserForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+// too define field types, you need too import the field type classes
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CreateUserForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['attr' => ['style' => 'color: grey;']])
+            ->add('email', EmailType::class, ['attr' => ['style' => 'color: grey;']])
+            ->add('password', PasswordType::class, ['attr' => ['style' => 'color: grey;']])
+            ->add('submit', SubmitType::class, ['attr' => ['style' => 'color: grey;']])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
