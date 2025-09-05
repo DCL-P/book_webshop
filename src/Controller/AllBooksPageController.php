58786cde30781a1cc21f058dc694a0527a829bd4
Richard <164242635+DCL-P@users.noreply.github.com>
2025-09-05 17:32:28 +0200

    edited AllBooksPage controller
    line 7-11+: added necessary use statements for form handling
    
    line 14-17+: added the newly imported components to the main controller class
    
    line 20-37+:
    
    Created the form needed for genre filtering. It fetches the http request, fetches the form data, and based on the next ifs executes the fetchBookByGenre repo method and binds the result to the AllBooks var

--- src/Controller/AllBooksPageController.php
+++ src/Controller/AllBooksPageController.php
@@ -4,19 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\BooksRepository;
+use App\Form\GenreFiltersForm;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AllBooksPageController extends AbstractController
 {
 
     #[Route('/all-books', name:'all-books-route')]
-    public function ShowAllBooksPage(BooksRepository $BooksRepository): Response
+    public function ShowAllBooksPage(Request $request, BooksRepository $BooksRepository, EntityManagerInterface $manager): Response
     {
 
-        //can't directly access like an array, since values are private
+        //all books list
         $AllBooks = $BooksRepository->FetchAllBooks();
 
-        return $this->render('all_books_page.html.twig', ['all_books_array' => $AllBooks]);
+        //genre filters
+        $form = $this->createForm(GenreFiltersForm::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $selectedGenres = $form->get('Genre')->getData();
+
+            if(!empty($selectedGenres)){
+                $filtered_genres_list = $BooksRepository->FetchBooksByGenre($selectedGenres);
+                $AllBooks = $filtered_genres_list;
+            }
+        }
+
+        return $this->render('all_books_page.html.twig', ['all_books_array' => $AllBooks, 'genre_filter' =>$form->createView()]);
     }
 }
