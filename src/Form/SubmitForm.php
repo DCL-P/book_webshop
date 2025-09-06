953e6ac6bac8d46b2254c8e9ef94d5f18eb2019b
Richard <164242635+DCL-P@users.noreply.github.com>
2025-09-06 19:33:15 +0200

    new Submit form
    This submit form will be used for temporary users to submit books to the temporary session book list

---
+++ src/Form/SubmitForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SubmitForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('submit_button', SubmitType::class, [
+                'label'=>'add'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
